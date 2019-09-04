639fa06061d0c3703131cc7ab17a2fb66dac7085
andrejcermak <andrej.cermak.1999@gmail.com>
2019-09-04 17:27:30 +0200

    CHANGED complete refactor

--- metadata/APIMetadata/Metadata.py
+++ metadata/APIMetadata/Metadata.py
@@ -1,57 +1,27 @@
-from Connection import connect
-from flask import session
-
-
 class Metadata:
-    def get(self, instance_id):
-        try:
-            token = session['token']
-            project_id = session['project_id']
-        except:
-            return {'message': 'unlogged'}, 401
-        try:
-            conn = connect(token, project_id)
-        except:
-            return {'message': 'connection error'}, 401
+    def get(self, connection, instance_id):
 
-        instance = conn.compute.find_server(instance_id)
+        instance = connection.compute.find_server(instance_id)
         if instance is None:
             return {"message": "instance not found"}, 400
-        return conn.compute.get_server_metadata(instance).to_dict(), 200
+        return connection.compute.get_server_metadata(instance).to_dict(), 200
 
-    def set(self, instance_id, metadata):
-        try:
-            token = session['token']
-            project_id = session['project_id']
-        except:
-            return {'message': 'unlogged'}, 401
-        try:
-            conn = connect(token, project_id)
-        except:
-            return {'message': 'connection error'}, 401
-        instance = conn.compute.find_server(instance_id)
+    def set(self, connection, instance_id, metadata):
+        instance = connection.compute.find_server(instance_id)
 
         if instance is None:
             return {"message": "instance not found"}, 400
-        return conn.compute.set_server_metadata(instance, **metadata)
+        return connection.compute.set_server_metadata(instance, **metadata)
 
-    def delete(self, instance_id, keys):
-        try:
-            token = session['token']
-            project_id = session['project_id']
-        except:
-            return {'message': 'unlogged'}, 401
-        try:
-            conn = connect(token, project_id)
-        except:
-            return {'message': 'connection error'}, 401
-        instance = conn.compute.find_server(instance_id)
+    def delete(self, connection, instance_id, keys):
+
+        instance = connection.compute.find_server(instance_id)
         if instance is None:
             return {"message": "instance not found"}, 400
-        metadata = conn.compute.get_server_metadata(instance).to_dict()["metadata"]
+        metadata = connection.compute.get_server_metadata(instance).to_dict()["metadata"]
         for key in keys:
             if metadata.get(key) is None:
                 return {"message": "key not in metadata"}, 400
 
-        conn.compute.delete_server_metadata(instance, keys)
+        connection.compute.delete_server_metadata(instance, keys)
         return {}, 204

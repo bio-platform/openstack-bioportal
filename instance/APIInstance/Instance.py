366f987599059b84b45b934c4be7bb28a30a37e8
andrejcermak <andrej.cermak.1999@gmail.com>
2019-09-04 17:31:06 +0200

    CHANGED complete refactor

--- instance/APIInstance/Instance.py
+++ instance/APIInstance/Instance.py
@@ -1,25 +1,13 @@
 from flask_restful import Resource
-from Connection import connect
-from flask import session
-import os
 from oslo_utils import encodeutils
 import base64
 
 
 class Instance(Resource):
 
-    def get(self, instance_id):
-        try:
-            token = session['token']
-            project_id = session['project_id']
-        except:
-            return {'message': 'unlogged'}, 401
-        try:
-            conn = connect(token, project_id)
-        except:
-            return {'message': 'connection error'}, 401
+    def get(self,connection, instance_id):
 
-        server = conn.compute.find_server(instance_id)
+        server = connection.compute.find_server(instance_id)
         if server is None:
             return {}, 404
         return server, 201
@@ -33,20 +21,13 @@ class Instance(Resource):
     def update(self):
         return {}, 501
 
-    def list(self):
-        try:
-            token = session['token']
-            project_id = session['project_id']
-        except:
-            return {'message': 'unlogged'}, 401
-        try:
-            conn = connect(token, project_id)
-        except:
-            return {'message': 'connection error'}, 401
-        tmp = conn.compute.servers()
+    def list(self, connection):
+
+        tmp = connection.compute.servers()
         return [r for r in tmp], 200
 
     def create(self,
+               connection,
                flavor,
                image,
                key_name,
@@ -56,20 +37,11 @@ class Instance(Resource):
                diskspace=None,
                volume_name=None
                ):
-        try:
-            token = session['token']
-            project_id = session['project_id']
-        except:
-            return {'message': 'unlogged'}, 401
-        try:
-            conn = connect(token, project_id)
-        except:
-            return {'message': 'connection error'}, 401
 
-        image = conn.compute.find_image(image)
-        flavor = conn.compute.find_flavor(flavor)
-        network = conn.network.find_network(network_id)
-        key_pair = conn.compute.find_keypair(key_name)
+        image = connection.compute.find_image(image)
+        flavor = connection.compute.find_flavor(flavor)
+        network = connection.network.find_network(network_id)
+        key_pair = connection.compute.find_keypair(key_name)
 
         with open("cloud-init-bioconductor-image.sh", "r") as file:
             text = file.read()
@@ -78,7 +50,7 @@ class Instance(Resource):
 
         if (image is None) or (flavor is None) or (network is None) or (key_pair is None):
             return {"message": "resource not found"}, 400
-        server = conn.compute.create_server(
+        server = connection.compute.create_server(
                 name=servername,
                 image_id=image.id,
                 flavor_id=flavor.id,

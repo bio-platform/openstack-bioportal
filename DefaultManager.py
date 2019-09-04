82fa9eed3614f9714224f5693a0d0f04a82335bb
andrejcermak <andrej.cermak.1999@gmail.com>
2019-09-04 17:18:41 +0200

    CHANGED complete refactor

--- DefaultManager.py
+++ DefaultManager.py
@@ -1,4 +1,7 @@
 from marshmallow import ValidationError
+from Connection import connect
+from flask import session
+
 
 def manage(fun,  json, schema=None, **kwargs):
 
@@ -6,10 +9,21 @@ def manage(fun,  json, schema=None, **kwargs):
         load = {}
         if schema is not None:
             load = schema().load(json)
-        return fun(**kwargs, **load)
+        try:
+            token = session['token']
+            project_id = session['project_id']
+        except:
+            return {'message': 'unlogged'}, 401
+        try:
+            connection = connect(token, project_id)
+        except:
+            return {'message': 'connection error'}, 401
+
+        return fun(connection, **kwargs, **load)
 
     except ValidationError as VE:
         return {'message': 'missing required arguments: ' + ', '.join(VE.messages), 'result': {}}, 400
+
 
 # def manage(fun,  request, schema=None, **kwargs):
 #

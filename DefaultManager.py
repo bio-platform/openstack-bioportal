0197011e109e4f0f4005e1a4011b273b38b680c9
cermak <andrej.cermak.1999@gmail.com>
2020-08-03 12:22:29 +0200

    CHANGED reformat and clean-up

--- DefaultManager.py
+++ DefaultManager.py
@@ -1,10 +1,10 @@
-from marshmallow import ValidationError
-from Connection import connect
 from flask import session
+from marshmallow import ValidationError
+
+from Connection import connect
 
 
-def manage(fun,  json, schema=None, **kwargs):
-
+def manage(fun, json, schema=None, **kwargs):
     try:
         load = {}
         if schema is not None:
@@ -23,17 +23,3 @@ def manage(fun,  json, schema=None, **kwargs):
 
     except ValidationError as VE:
         return {'message': 'missing required arguments: ' + ', '.join(VE.messages), 'result': {}}, 400
-
-
-# def manage(fun,  request, schema=None, **kwargs):
-#
-#     try:
-#         if schema is None:
-#             schema = DefaultSchema
-#         json = request.json
-#         load = schema().load(request.json)
-#         return fun(**kwargs, **load)
-#
-#     except ValidationError as VE:
-#         return {'message': 'missing required arguments: ' + ', '.join(VE.messages), 'result': {}}, 400
-#

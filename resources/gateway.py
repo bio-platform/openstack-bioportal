b971f49e7e4cbf4d62f638954e946e07815369d2
cermak <andrej.cermak.1999@gmail.com>
2020-08-07 17:37:08 +0200

    ADDED docs

--- resources/gateway.py
+++ resources/gateway.py
@@ -10,6 +10,24 @@ class Gateway(Resource):
 
     @staticmethod
     def put(router_id):
+        """
+            **Update gateway**
+            This function allows users to add external gateway.
+            Its json input is specified by schema.NetworkSchema
+            :return: router information in json and http status code
+            - Example::
+                  curl -X GET bio-portal.metacentrum.cz/api/gateways/ -H 'Cookie: cookie from scope' -H
+                  'content-type: application/json' --data json specified in schema
+
+            - Expected Success Response::
+                HTTP Status Code: 200
+                json-format: see openstack.compute.v2.server
+
+            - Expected Fail Response::
+                HTTP Status Code: 400
+                {"message": "Wrong router ID, router not found!"}
+
+        """
         connection = connect(session["token"], session["project_id"])
         load = NetworkSchema().load(request.json)
         router = connection.network.find_router(router_id)

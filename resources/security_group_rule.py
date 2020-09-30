80fad0bd25e54b9a738ceeaa9e30570f01da3551
cermak <andrej.cermak.1999@gmail.com>
2020-09-30 14:42:57 +0200

    ADDED http and https rules

--- resources/security_group_rule.py
+++ resources/security_group_rule.py
@@ -59,6 +59,26 @@ class SecurityGroupRule(Resource):
                 ether_type="IPv4",
                 remote_ip_prefix="0.0.0.0/0")
 
+        if load["type"] == "http":
+            new_rule = connection.network.create_security_group_rule(
+                direction="ingress",
+                protocol="tcp",
+                security_group_id=security_group_id,
+                ether_type="IPv4",
+                port_range_min="80",
+                port_range_max="80",
+                remote_ip_prefix="0.0.0.0/0")
+
+        if load["type"] == "https":
+            new_rule = connection.network.create_security_group_rule(
+                direction="ingress",
+                protocol="tcp",
+                port_range_min="443",
+                port_range_max="443",
+                security_group_id=security_group_id,
+                ether_type="IPv4",
+                remote_ip_prefix="0.0.0.0/0")
+
         return new_rule.to_dict(), 201
 
 

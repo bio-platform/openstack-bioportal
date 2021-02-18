eba185d8a4364192c048b3a2117e3482cd949e2d
cermak <andrej.cermak.1999@gmail.com>
2021-02-18 17:28:43 +0100

    Adding task class with result parsing

--- resources/instance2.py
+++ resources/instance2.py
@@ -3,10 +3,8 @@
 .. moduleauthor:: Andrej Cermak <github.com/andrejcermak>
 """
 
-import base64
-
-from oslo_utils import encodeutils
-
+import regex
+from json import JSONDecoder as decoder
 from flask import request
 from flask_restful import Resource
 from flask import session as flask_session
@@ -15,10 +13,9 @@ from schema import StartServerSchema
 from Connection import connect
 import requests
 
-from python_terraform import Terraform
-import regex
 
 class Instance2(Resource):
+
 
     @staticmethod
     def post():
@@ -30,15 +27,8 @@ class Instance2(Resource):
         network = connection.network.find_network(data["network_id"])
         key_pair = connection.compute.find_keypair(data["key_name"])
 
-        req = requests.get(
-            "https://raw.githubusercontent.com/bio-platform/bio-class/master/install/cloud-init-bioconductor-image.sh")
-        text = encodeutils.safe_encode(req.text.encode("utf-8"))
-        init_script = base64.b64encode(text).decode("utf-8")
-
         if (image is None) or (flavor is None) or (network is None) or (key_pair is None):
             return {"message": "resource not found"}, 400
-
-        from app import app
 
         user_variables = {
             "instance_name": "data",
@@ -47,19 +37,38 @@ class Instance2(Resource):
             "user_name": data["metadata"]["name"],
             "user_email": data["metadata"]["email"],
             "floating_ip": "78.128.250.94",
-            "init_script": init_script
+            "token": connection.authorize()
         }
-        response = requests.post("http://127.0.0.1:5000/api/v1/configurations/with_vars/apply?async",
-                             headers={'Authorization': 'Token token'},
+        response = requests.post("http://terrestrial_api_1:8000/api/v1/configurations/bioconductor/apply?async",
+                             headers={'Authorization': 'Token dev'},
                              data=user_variables)
-        print("api:", response.status_code, type(response.status_code))
         return {"id": response.content.decode()}, response.status_code
 
-class Instance2Task(Resource):
+
+class Task(Resource):
+
     @staticmethod
     def get(task_id):
-        print("aa", task_id)
         connection = connect(flask_session['token'], flask_session['project_id'])
-        response = requests.get("http://127.0.0.1:5000/api/v1/tasks/"+task_id,
-                                 headers={'Authorization': 'Token token'})
-        return response.content, response.status_code
+        response = requests.get("http://terrestrial_api_1:8000/api/v1/tasks/"+task_id,
+                                 headers={'Authorization': 'Token dev'})
+        state = response.content.decode()
+
+        if state == "PENDING":
+            return {"state": state, "reason": {}, "log": ""}, 200
+
+        if state == "SUCCESS":
+            result = requests.get("http://localhost:8000/api/v1/tasks/%s/result" % task_id,
+                                  headers={'Authorization': 'Token dev'}).content.decode()
+            if result.find("Apply complete!") != -1:
+                return {"state": "success", "reason": {}, "log": result}, 201
+
+            if result.find("Error creating OpenStack") != -1:
+                pattern = regex.compile(r'\{(?:[^{}]|(?R))*\}')
+                print("reason", result)
+                reason, _ = decoder().raw_decode(pattern.findall(result)[0])
+                keys = [i for i in reason.keys()]
+                return {"state": "error", "reason": reason[keys[0]], "log": result}, 201
+
+        return {"state": state, "reason": {}, "log": ""}, 200
+

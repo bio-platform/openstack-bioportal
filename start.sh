aae6f166dd88c7b2a69392b91bb3bd20e07e85f0
Andrej Čermák <andrej.cermak.1999@gmail.com>
2021-02-18 18:59:23 +0100

    Fixing gunicorn

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 service nginx start
-gunicorn -b 127.0.0.1:4000 app:app --log-file gunicorn.log --log-level DEBUG
+gunicorn -b 127.0.0.1:4000 app:app --log-file /var/log/bioportal/gunicorn.log --log-level DEBUG

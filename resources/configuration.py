32e702784830341985a1f092a2d9b4cd89075991
cermak <andrej.cermak.1999@gmail.com>
2021-07-14 11:24:27 +0200

    Changing API according to frontend needs

--- resources/configuration.py
+++ resources/configuration.py
@@ -12,18 +12,10 @@ class Configuration(Resource):
     def get(name=None):
         configs = requests.get("https://raw.githubusercontent.com/bio-platform/"
                                 "bioportal_configs/master/configurations.json").json()
-        names=[]
         if name is None:
-            for machine in configs["machines"]:
-                names.append(machine["name"])
-            return names, 200
-
-        res = None
+            return configs["machines"], 200
 
         for machine in configs["machines"]:
             if machine["name"] == name:
-                res = machine
-        configs["machines"] = [res]
-        if res is None:
-            return {}, 404
-        return res, 200
+                return machine, 200
+        return {}, 404
